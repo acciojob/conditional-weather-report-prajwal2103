9204378f6db095ac17fe307316aa0970482a82ed
prajwal2103 <prajwaljagtap5@gmail.com>
2024-09-03 13:07:31 +0530

    Submit3

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,8 @@
+
+
+import React from "react";
+import './../styles/App.css';
+
 const App = () => {
 
   const weather = { temperature:25, conditions: "Sunny"};
@@ -11,3 +16,5 @@ const App = () => {
     </div>
   )
 }
+
+export default App
