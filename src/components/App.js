bb74f1861d0e5720670dd070aa477b17555cfa4d
prajwal2103 <prajwaljagtap5@gmail.com>
2024-09-03 12:50:28 +0530

    Submit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,26 @@
 
 import React from "react";
 import './../styles/App.css';
+import { useState,useEffect } from "react";
+import WeatherDisplay from "./WeatherDisplay";
 
 const App = () => {
+  const [input, setInput] = useState({ temperature: 25, conditions: "Sunny" });
+  const[textColor,setColor]=useState('blue');
+  useEffect(()=>{
+      if(input.temperature>20){
+       setColor('red')
+      }
+      else{
+       setColor('blue')
+      }
+   
+   
+     },[temperature])
+  
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div >
+     <WeatherDisplay weatherObj={input}  textColor={textColor}/>
     </div>
   )
 }
---
+++ src/components/WeatherDisplay.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+const WeatherDisplay = ({weatherObj,textColor}) => {
+  
+  return (
+    <div style={{color:textColor}}>
+      <p>Temperature: ${weatherObj.temperature}</p>
+      <p>Conditions: ${weatherObj.conditions}</p>
+    </div>
+  )
+}
+
+export default WeatherDisplay
